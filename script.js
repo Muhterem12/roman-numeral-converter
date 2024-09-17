cf88593a624b4f165815e28e7d1092fbde58d501
Muhterem12 <alkanahmetmuho@gmail.com>
2024-09-17 09:13:49 +0300

    v3

--- script.js
+++ script.js
@@ -26,8 +26,28 @@ input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') output.click();
 });
 
-output.addEventListener('click', () => {
+btn.addEventListener('click', () => {
   const value = input.value;
 
   if (!value) output.innerText = 'Please enter a valid number';
+  console.log('success1');
 });
+
+// console.log(Object.keys(romanNumbers));
+
+btn.addEventListener('click', (e) => {
+  for (let i of Object.keys(romanNumbers)) {
+    // console.log(romanNumbers);
+
+    const cur = input.value;
+    converter(cur);
+
+    console.log(romanNumbers[i]);
+    console.log(i);
+    console.log('success2');
+  }
+});
+
+const converter = function (num) {
+  console.log(num);
+};
